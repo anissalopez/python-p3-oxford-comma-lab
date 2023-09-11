04e0c3399ab3a650ff889723c2892afab21c3525
anissalopez <anissa965@gmail.com>
2023-09-11 13:34:17 -0400

    oxford comma

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,17 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return " and ".join(items)
+    else:
+        new_list = items[:-1]
+        end_item = items[-1]
+        return ", ".join(new_list) + ", and " + end_item
+
+
+
+
+            
+
+
+print(oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]))
